e3c2689b1f61bffddd81676601abdd96f3f70baf
Vitor Baptista <vitor@vitorbaptista.com>
2012-07-04 21:37:40 -0300

    Show blip info on sidebar on click

--- radar.js
+++ radar.js
@@ -85,12 +85,14 @@ Radar = (function (){
         .attr('y', function (blip){
           return center.y + _toRect(blip.pc).y;
         })
-        .attr('title', function (blip) { return blip.name });
+        .attr('title', function (blip) { return blip.name })
+        .on('click', _showBlipDescription);
 
     blip.append('text')
         .text(function (blip, index) { return index; })
         .style('opacity', '0')
         .attr('class', 'label')
+        .on('click', _showBlipDescription)
         .attr('transform', function (blip) {
           var blipCenter = _toRect(blip.pc);
           return 'translate(' + (center.x+blipCenter.x+5) + ', ' + (center.y+blipCenter.y-2) + ')';
@@ -98,6 +100,11 @@ Radar = (function (){
 
     return svg;
   }
+
+  function _showBlipDescription(blip) {
+    $('#blip-name').text(blip.name);
+    $('#blip-description').text(blip.description);
+  };
 
   function _centerOf(d3_element) {
     var element = document.getElementById(d3_element.attr('id')),
